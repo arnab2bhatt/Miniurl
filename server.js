677efacc73ddac88f31648439762739bc92a5d45
arnab2bhatt <arnab.bhatt2003@gmail.com>
2024-10-25 16:29:10 +0530

    edit

--- server.js
+++ server.js
@@ -27,5 +27,5 @@ app.use('/links/analytics',authenticateJWT, anaRoutes); // Assuming the analytic
 
 // Start the server
 app.listen(port, '0.0.0.0', () => {
-    console.log(`Server running at http://0.0.0.0:${port}`);
+    console.log(`Server running at http://localhost:${port}`);
 });
